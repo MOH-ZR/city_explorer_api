5ea3b0819ba5260ffde4315fcfdcf95abe73ba94
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-03-30 19:01:56 +0300

    lab-08 done

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ const cors = require('cors');
 // Application Setup
 const PORT = process.env.PORT;
 const GEO_CODE_API_KEY = process.env.GEO_CODE_API_KEY;
-const WEATHER_CODE_API_KEY = process.env.WEATHER_CODE_API_KEY;
-const PARKS_CODE_API_KEY = process.env.PARKS_CODE_API_KEY;
 const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
@@ -24,8 +22,6 @@ const client = new pg.Client(DATABASE_URL);
 
 // routes
 app.get('/location', handleLocationRequest);
-app.get('/weather', handleWeatherRequest);
-app.get('/parks', handleParksRequest);
 app.get('/', (request, response) => { response.status(200).send('ok'); });
 app.use('*', notFoundHandler);
 
@@ -68,37 +64,6 @@ function handleLocationRequest(req, res) {
 
 }
 
-function handleWeatherRequest(req, res) {
-    const searchQueryLat = req.query.lat;
-    const searchQueryLon = req.query.lon;
-    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${searchQueryLat}&lon=${searchQueryLon}&key=${WEATHER_CODE_API_KEY}&include=minutely`;
-
-    superagent.get(url).then(resData => {
-        const weatherMap = resData.body.data.map((day) => {
-            return new Weather(day);
-        });
-        res.status(200).send(weatherMap);
-    }).catch((error) => {
-        res.status(500).send('Sorry, something went wrong');
-    });
-}
-
-
-function handleParksRequest(req, res) {
-    const cityName = req.query.location;
-    const url = `https://developer.nps.gov/api/v1/parks?location=${cityName}&api_key=${PARKS_CODE_API_KEY}`;
-    superagent.get(url).then(resData => {
-        const tenParks = resData.body.data.map((park) => {
-            return new Park(park);
-        });
-        res.status(200).send(tenParks.slice(0, 10));
-    }).catch((error) => {
-        res.status(500).send('Sorry, something went wrong');
-    });
-
-}
-
-
 // constructors
 function Location(data, query) {
     this.search_query = query;
@@ -107,23 +72,10 @@ function Location(data, query) {
     this.longitude = data.lon;
 }
 
-function Weather(day) {
-    this.forecast = day.weather.description;
-    this.time = day.datetime;
-}
-
-function Park(parkData) {
-    this.name = parkData.fullName;
-    this.address = parkData.addresses[0].postalCode + " " + parkData.addresses[0].line1 + ", " + parkData.addresses[0].city + ", " + parkData.addresses[0].stateCode;
-    this.fee = "0.00"; // Note: I used parkData.fees to get back the fees but since it's value is an emty arry I used "0.00" instead.
-    this.description = parkData.description;
-    this.url = parkData.url;
-}
 
 function notFoundHandler(request, response) {
     response.status(404).send('Not found!');
 }
-
 
 // Connect to DB and Start the Web Server
 client.connect().then(() => {
