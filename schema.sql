6b9267e37bd779908c6cb49bb1aad554dbf43415
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-03-30 18:19:29 +0300

    lab-08 done

---
+++ schema.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE location (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    fullname VARCHAR(255),
+    latitude VARCHAR(255),
+    longitude VARCHAR(255)
+);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
+const pg = require('pg');
 const cors = require('cors');
 
 // Application Setup
@@ -13,34 +14,58 @@ const PORT = process.env.PORT;
 const GEO_CODE_API_KEY = process.env.GEO_CODE_API_KEY;
 const WEATHER_CODE_API_KEY = process.env.WEATHER_CODE_API_KEY;
 const PARKS_CODE_API_KEY = process.env.PARKS_CODE_API_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const app = express();
 app.use(cors());
+
+// Database Connection Setup
+const client = new pg.Client(DATABASE_URL);
 
 // routes
 app.get('/location', handleLocationRequest);
 app.get('/weather', handleWeatherRequest);
 app.get('/parks', handleParksRequest);
+app.get('/', (request, response) => { response.status(200).send('ok'); });
 app.use('*', notFoundHandler);
 
 
 function handleLocationRequest(req, res) {
-    const searchQuery = req.query.city;
-    const url = `https://us1.locationiq.com/v1/search.php?key=${GEO_CODE_API_KEY}&city=${searchQuery}&format=json`;
-    if (!searchQuery) {
+    const cityName = req.query.city;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${GEO_CODE_API_KEY}&city=${cityName}&format=json`;
+    if (!cityName) {
         res.status(404).send('no search query was provided');
     }
-    const cityQueryParam = {
-        key: GEO_CODE_API_KEY,
-        city: searchQuery,
-        format: 'json'
-    };
-    superagent.get(url).query(cityQueryParam).then(resData => {
-        const location = new Location(resData.body[0], searchQuery);
-        res.status(200).send(location);
+
+    // check if the city is exist in the city database
+    const safeValues = [cityName];
+    const sqlQuery = `SELECT * FROM location WHERE name=$1`;
+
+    // query the database
+    client.query(sqlQuery, safeValues).then(result => {
+        if (result.rows.length === 0) {
+            throw error;
+        }
+        res.status(200).json(result.rows[0]);
     }).catch((error) => {
-        res.status(500).send('Sorry, something went wrong');
+        const cityQueryParam = {
+            key: GEO_CODE_API_KEY,
+            city: cityName,
+            format: 'json'
+        };
+
+        superagent.get(url).query(cityQueryParam).then(resData => {
+            const location = new Location(resData.body[0], cityName);
+            const safeValues = [cityName, location.formatted_query, location.latitude, location.longitude];
+            const sqlQuery = `INSERT INTO location(name, fullname, latitude, longitude) VALUES ($1, $2, $3, $4)`;
+            client.query(sqlQuery, safeValues);
+
+            res.status(200).send(location);
+        }).catch((error) => {
+            res.status(500).send('Sorry, something went wrong');
+        });
     });
+
 }
 
 function handleWeatherRequest(req, res) {
@@ -57,10 +82,11 @@ function handleWeatherRequest(req, res) {
         res.status(500).send('Sorry, something went wrong');
     });
 }
-// Yellowstone
+
+
 function handleParksRequest(req, res) {
-    const searchQuery = req.query.location;
-    const url = `https://developer.nps.gov/api/v1/parks?location=${searchQuery}&api_key=${PARKS_CODE_API_KEY}`;
+    const cityName = req.query.location;
+    const url = `https://developer.nps.gov/api/v1/parks?location=${cityName}&api_key=${PARKS_CODE_API_KEY}`;
     superagent.get(url).then(resData => {
         const tenParks = resData.body.data.map((park) => {
             return new Park(park);
@@ -98,6 +124,11 @@ function notFoundHandler(request, response) {
     response.status(404).send('Not found!');
 }
 
-app.listen(PORT, () => {
-    console.log("litening...");
-});
+
+// Connect to DB and Start the Web Server
+client.connect().then(() => {
+    app.listen(PORT, () => {
+        console.log("Connected to database:", client.connectionParameters.database) //show what database we connected to
+        console.log('Server up on', PORT);
+    });
+})
