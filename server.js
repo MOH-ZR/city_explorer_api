ff0cbe67fbb7cf1f0c0ab4ca9ef84a271b2b13cb
zaa92reer@gmail.com <mohammad@Moh_Zar.localdomain>
2021-04-01 12:47:56 +0300

    lab-09 done

--- server.js
+++ server.js
@@ -6,81 +6,79 @@ require('dotenv').config();
 // Application Dependencies
 const express = require('express');
 const superagent = require('superagent');
-const pg = require('pg');
 const cors = require('cors');
 
 // Application Setup
 const PORT = process.env.PORT;
-const GEO_CODE_API_KEY = process.env.GEO_CODE_API_KEY;
-const DATABASE_URL = process.env.DATABASE_URL;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const YELP_API_KEY = process.env.YELP_API_KEY;
 
 const app = express();
 app.use(cors());
 
-// Database Connection Setup
-const client = new pg.Client(DATABASE_URL);
-
 // routes
-app.get('/location', handleLocationRequest);
-app.get('/', (request, response) => { response.status(200).send('ok'); });
+app.get('/movies', handleMovieRequest);
+app.get('/yelp', handleRestRequest);
 app.use('*', notFoundHandler);
 
 
-function handleLocationRequest(req, res) {
-    const cityName = req.query.city;
-    const url = `https://us1.locationiq.com/v1/search.php?key=${GEO_CODE_API_KEY}&city=${cityName}&format=json`;
-    if (!cityName) {
+// handler functions
+function handleRestRequest(req, res) {
+    const area = req.query.;
+    const url = ``;
+    if (!area) {
         res.status(404).send('no search query was provided');
     }
+    const areaQuery = {
+        "key": YELP_API_KEY,
+        "area": area
+    };
 
-    // check if the city is exist in the city database
-    const safeValues = [cityName];
-    const sqlQuery = `SELECT * FROM location WHERE name=$1`;
-
-    // query the database
-    client.query(sqlQuery, safeValues).then(result => {
-        if (result.rows.length === 0) {
-            throw error;
-        }
-        res.status(200).json(result.rows[0]);
+    superagent.get(url).set({ 'Authorization': 'Bearer ' + YELP_API_KEY }).query(areaQuery).then(resData => {
+        const restaurant = new Restaurant(resData.body[0]);
+        res.status(200).send(restaurant);
     }).catch((error) => {
-        const cityQueryParam = {
-            key: GEO_CODE_API_KEY,
-            city: cityName,
-            format: 'json'
-        };
-
-        superagent.get(url).query(cityQueryParam).then(resData => {
-            const location = new Location(resData.body[0], cityName);
-            const safeValues = [cityName, location.formatted_query, location.latitude, location.longitude];
-            const sqlQuery = `INSERT INTO location(name, fullname, latitude, longitude) VALUES ($1, $2, $3, $4)`;
-            client.query(sqlQuery, safeValues);
-
-            res.status(200).send(location);
-        }).catch((error) => {
-            res.status(500).send('Sorry, something went wrong');
-        });
+        res.status(500).send('Sorry, something went wrong');
     });
-
 }
 
-// constructors
-function Location(data, query) {
-    this.search_query = query;
-    this.formatted_query = data.display_name;
-    this.latitude = data.lat;
-    this.longitude = data.lon;
-}
+function handleMovieRequest(req, res) {
+    const area = req.query.;
+    const url = ``;
+    if (!area) {
+        res.status(404).send('no search query was provided');
+    }
+    const areaQuery = {
+        "key": MOVIE_API_KEY,
+        "area": area
+    };
 
+    superagent.get(url).query(areaQuery).then(resData => {
+        const movie = new Movie(resData.body[0]);
+        res.status(200).send(movie);
+    }).catch((error) => {
+        res.status(500).send('Sorry, something went wrong');
+    });
+}
 
 function notFoundHandler(request, response) {
     response.status(404).send('Not found!');
 }
+// constructors
+function Restaurant() {
+    this.name = ;
+    this.image_url = ;
+    this.price = ;
+    this.rating = ;
+    this.url = ;
+}
 
-// Connect to DB and Start the Web Server
-client.connect().then(() => {
-    app.listen(PORT, () => {
-        console.log("Connected to database:", client.connectionParameters.database) //show what database we connected to
-        console.log('Server up on', PORT);
-    });
-})
+function Movie() {
+    this.title = ;
+    this.overview = ;
+    this.average_votes = ;
+    this.total_votes = ;
+    this.image_url = ;
+    this.popularity = ;
+    this.released_on = ;
+}
